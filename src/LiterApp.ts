88cb5451b18d5567343f2b68be2a0a97b0481ae6
Electrolyte Orb <electrolyte.orb@gmail.com>
2021-03-16 17:43:45 +0530

    removed boilerplate code

--- src/LiterApp.ts
+++ src/LiterApp.ts
@@ -1,56 +1,11 @@
-import { LitElement, html, css, property } from 'lit-element';
-import { openWcLogo } from './open-wc-logo.js';
+import { LitElement, html, property } from 'lit-element';
 
 export class LiterApp extends LitElement {
   @property({ type: String }) title = 'My app';
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--liter-app-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo > svg {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
-
   render() {
     return html`
       <main>
-        <div class="logo">${openWcLogo}</div>
         <h1>${this.title}</h1>
 
         <p>Edit <code>src/LiterApp.ts</code> and save to reload.</p>
